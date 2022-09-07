b2db52d844fceb8f40ae87cf18e56b2370813e6b
arvidhyapriyadarshini <112971590+arvidhyapriyadarshini@users.noreply.github.com>
2022-09-07 14:14:27 -0400

    Update displayResults.py

--- displayResults.py
+++ displayResults.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def readLatestEntry():
-    with open('C:\\DataPythonProject\\randomTempDict3.txt', 'r') as file:
+    with open('https://github.com/arvidhyapriyadarshini/datadisplayResultV01/blob/main/randomTempDict3.txt', 'r') as file:
         for last_line in file:
             pass
 
